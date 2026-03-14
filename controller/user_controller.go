0417fd5b9b555500ea2f468bb04ccebd74fc5c0c
sidz111 <sssurwade2212@gmail.com>
2026-03-14 23:29:42 +0530

    worked on userController

---
+++ controller/user_controller.go
@@ -0,0 +1,104 @@
+package controller
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sidz111/jwt-twitter-msg/models"
+	"github.com/sidz111/jwt-twitter-msg/service"
+)
+
+type UserController struct {
+	serv service.UserService
+}
+
+func NewUserController(serv service.UserService) *UserController {
+	return &UserController{serv: serv}
+}
+
+func (c *UserController) CreateUser(ctx *gin.Context) {
+	var user *models.User
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	if err := c.serv.CreateUser(ctx.Request.Context(), user); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	ctx.JSON(http.StatusCreated, gin.H{
+		"user": user,
+	})
+}
+func (c *UserController) GetUser(ctx *gin.Context) {
+	id, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	user, err := c.serv.GetUser(ctx.Request.Context(), uint(id))
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{
+		"user": user,
+	})
+}
+func (c *UserController) GetAllUsers(ctx *gin.Context) {
+	users, err := c.serv.GetAllUsers(ctx.Request.Context())
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{
+		"users": users,
+	})
+}
+func (c *UserController) UpdateUser(ctx *gin.Context) {
+	var user models.User
+	if err := ctx.ShouldBindJSON(&user); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	if err := c.serv.UpdateUser(ctx, &user); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "user updated successfully",
+	})
+}
+func (c *UserController) DeleteUser(ctx *gin.Context) {
+	id, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	if err := c.serv.DeleteUser(ctx.Request.Context(), uint(id)); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "user deleted successfully",
+	})
+}
