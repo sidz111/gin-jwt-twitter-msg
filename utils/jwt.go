32143138c3abd5bcc19a8437b5f0414dfc4b0aac
sidz111 <sssurwade2212@gmail.com>
2026-03-15 12:52:00 +0530

    token generated

---
+++ utils/jwt.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var SECRET_KEY = []byte("supersecretkey")
+
+func GenerateJWT(username string, userID uint) (string, error) {
+	claims := jwt.MapClaims{
+		"username": username,
+		"user_id":  userID,
+		"exp":      time.Now().Add(time.Hour * 24).Unix(),
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(SECRET_KEY)
+	if err != nil {
+		return "no token", err
+	}
+	return tokenString, nil
+}
