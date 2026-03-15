a0acaff366e3c747491a8549f23b4dc26e899f02
sidz111 <sssurwade2212@gmail.com>
2026-03-15 22:19:17 +0530

    implements auth middleware manually with jwt

---
+++ middleware/auth_middleware.go
@@ -0,0 +1,38 @@
+package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var SECRET_KEY = []byte("supersecretkey")
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+
+		if authHeader == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "Empty Token",
+			})
+			c.Abort()
+			return
+		}
+		tokenString := strings.Split(authHeader, " ")[1]
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			return SECRET_KEY, nil
+		})
+
+		if err != nil || !token.Valid {
+			c.JSON(http.StatusUnauthorized, gin.H{
+				"error": "User Not Valid",
+			})
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
