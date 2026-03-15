0979ef6f5df84298e8fb58e37319b30b77883626
sidz111 <sssurwade2212@gmail.com>
2026-03-15 12:34:55 +0530

    add jwt token un user

--- models/user.go
+++ models/user.go
@@ -9,6 +9,7 @@ type User struct {
 	Username string `gorm:"size:50;uniqueIndex;not null" json:"username"`
 	Email    string `gorm:"size:100;uniqueIndex;not null" json:"email"`
 	Password string `gorm:"size:255;not null" json:"-"`
+	Token    string `json:"token"` //bad practice to save this field in db, i'll manage it after testing
 
 	Bio string `gorm:"type:text" json:"bio"`
 
