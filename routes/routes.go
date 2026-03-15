deda51027fb9310e0f37274df026d4b8ed4a2c74
sidz111 <sssurwade2212@gmail.com>
2026-03-15 22:22:22 +0530

    passed middleware in rest api's

--- routes/routes.go
+++ routes/routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/sidz111/jwt-twitter-msg/controller"
+	"github.com/sidz111/jwt-twitter-msg/middleware"
 )
 
 func SetRoutes(userController *controller.UserController, postController *controller.PostController, router *gin.Engine) *gin.Engine {
@@ -10,20 +11,20 @@ func SetRoutes(userController *controller.UserController, postController *contro
 	user := r.Group("users")
 	{
 		user.POST("/", userController.CreateUser)
-		user.GET("/:id", userController.GetUser)
-		user.GET("/", userController.GetAllUsers)
-		user.PUT("/", userController.UpdateUser)
-		user.DELETE("/:id", userController.DeleteUser)
+		user.GET("/:id", middleware.AuthMiddleware(), userController.GetUser)
+		user.GET("/", middleware.AuthMiddleware(), userController.GetAllUsers)
+		user.PUT("/", middleware.AuthMiddleware(), userController.UpdateUser)
+		user.DELETE("/:id", middleware.AuthMiddleware(), userController.DeleteUser)
 	}
 
 	post := r.Group("posts")
 	{
-		post.POST("/", postController.CreatePost)
-		post.GET("/:id", postController.GetPost)
-		post.GET("/", postController.GetAllPosts)
-		post.PUT("/", postController.UpdatePost)
-		post.DELETE("/:id", postController.DeletePost)
-		post.GET("/users/:userID", postController.GetPostsByUserId)
+		post.POST("/", middleware.AuthMiddleware(), postController.CreatePost)
+		post.GET("/:id", middleware.AuthMiddleware(), postController.GetPost)
+		post.GET("/", middleware.AuthMiddleware(), postController.GetAllPosts)
+		post.PUT("/", middleware.AuthMiddleware(), postController.UpdatePost)
+		post.DELETE("/:id", middleware.AuthMiddleware(), postController.DeletePost)
+		post.GET("/users/:userID", middleware.AuthMiddleware(), postController.GetPostsByUserId)
 	}
 	return r
 }
